eff9b8b4fcbc3f01e6ff0f5b76b0909565ef2b95
tobenot <2818373670@qq.com>
2024-10-22 03:43:13 +0800

    feat: asset

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  siteTitle: "我的作品集",
+  siteTitle: "tobenot的作品集/墓志铭 庆祝2024年现实生日而设计",
   navItems: [
     { name: "首页", path: "/" },
     { name: "关于我", path: "/about" }
@@ -7,26 +7,122 @@ export default {
   projects: [
     {
       id: 1,
-      title: "项目1",
-      image: "/images/project1.jpg",
-      description: "这是项目1的描述",
-      link: "https://project1.com"
+      title: "作家之屋",
+      image: require('@/assets/images/teacat.webp'),
+      description: "博客，【成为人类】事件的主要促进者。",
+      link: "https://tobenot.top/"
     },
     {
       id: 2,
-      title: "项目2",
-      image: "/images/project2.jpg",
-      description: "这是项目2的描述",
-      link: "https://project2.com"
+      title: "猎蝗人",
+      image: require('@/assets/images/lh_header_schinese.jpg'),
+      description: "第三人称关卡制清版太空射击游戏。大二的作品，我与音乐师制作，外援建模。",
+      link: "https://store.steampowered.com/app/3008350/"
+    },
+    {
+      id: 4,
+      title: "夜酱：沉默杀手",
+      image: require('@/assets/images/nbsk_header.jpg'),
+      description: "第三人称动作游戏，大一下的作品，我与音乐师制作，外援建模。",
+      link: "https://store.steampowered.com/app/1494180/Night_Silent_Killer/"
+    },
+    {
+      id: 11,
+      title: "不止于纸上的故事：鸿律篇",
+      image: require('@/assets/images/bbr_title.png'),
+      description: "已经完成的作为礼物的小说，预备借助AI来动画化。",
+      link: "https://www.bilibili.com/video/BV1gk2PY9E9C/"
+    },
+    {
+      id: 12,
+      title: "不止于纸上的故事",
+      image: require('@/assets/images/bbw.png'),
+      description: "大模型跑团游戏，网页端。",
+      link: "https://bb.tobenot.top/main/"
+    },
+    {
+      id: 6,
+      title: "挑战之旅",
+      image: require('@/assets/images/co_header.png'),
+      description: "毕设，ARPG+大模型AI游戏，单人制作。不推荐游玩。",
+      link: "https://store.steampowered.com/app/2778690/Challengers_Odyssey/"
+    },
+    {
+      id: 14,
+      title: "UE5大模型游戏插件",
+      image: require('@/assets/images/co_plugin.png'),
+      description: "毕设用的，其实啥游戏都可以用。后续有个大项目就还想用。",
+      link: "https://github.com/tobenot/TobenotLLMGameplay"
+    },
+    {
+      id: 5,
+      title: "心灵世界",
+      image: require('@/assets/images/hw_header_schinese.jpg'),
+      description: "第一人称砍杀游戏，高考后寒假和大一上的作品。本来想做成代表作，结果效果并不好，很希望能重制。不推荐游玩。",
+      link: "https://store.steampowered.com/app/1415580/Heart_World/"
+    },
+    {
+      id: 3,
+      title: "极北之星",
+      image: require('@/assets/images/ns_header.jpg'),
+      description: "处女作。2018年高二的作品，与几个同学们一起制作。全是策划。",
+      link: "https://store.steampowered.com/app/843050/North_Stars/"
+    },
+    {
+      id: 7,
+      title: "文明史诗",
+      image: require('@/assets/images/civ.png'),
+      description: "一款弹幕互动联机的AI图文文明发展演化游戏。",
+      link: "https://play-live.bilibili.com/details/1729301917890"
+    },
+    {
+      id: 8,
+      title: "璀璨群星",
+      image: require('@/assets/images/BS.png'),
+      description: "一款类群星弹幕互动游戏。拿了B站的最佳弹幕游戏第二名。目前不可玩。",
+      link: "https://www.bilibili.com/video/BV1c5411D7Qb/"
+    },
+    {
+      id: 16,
+      title: "智能荣光",
+      image: require('@/assets/images/ig.png'),
+      description: "对战的弹幕互动游戏，因为觉得UE5那个水体插件很好看就做了。目前不可玩。",
+      link: "https://www.bilibili.com/video/BV1o34y1h7Pu/"
+    },
+    {
+      id: 9,
+      title: "舰队激斗",
+      image: require('@/assets/images/fb.png'),
+      description: "一款召唤战舰对战的弹幕互动游戏。赚了4k和五个舰长。目前不可玩。",
+      link: "https://www.bilibili.com/video/BV1k34y1e7Ew/"
+    },
+    {
+      id: 10,
+      title: "操场·高楼·太空电梯",
+      image: require('@/assets/images/playground.webp'),
+      description: "小说。《不止于纸上的故事》中的一个短篇。",
+      link: "https://www.bilibili.com/read/cv11529157/"
+    },
+    {
+      id: 13,
+      title: "在线语音合成",
+      image: require('@/assets/images/tts.png'),
+      description: "在线语音合成，基于微软Edge。",
+      link: "https://tobenot.top/simple-tts-js/"
+    },
+    {
+      id: 15,
+      title: "不止于纸上的故事：幻灭篇",
+      image: require('@/assets/images/bbd.png'),
+      description: "本来想做严肃游戏，讲高中学习方法和心界起源的，暂时没空做。",
+      link: "https://tobenot.top/Beyond-Books-Disillusionment/"
     }
-    // 您可以在这里添加更多项目
   ],
   about: {
     title: "关于我",
-    content: "这里是您的个人介绍...",
+    content: "看这里不如看博客？",
     personalWorks: [
-      { title: "作品1", image: "/images/work1.jpg" },
-      { title: "作品2", image: "/images/work2.jpg" }
+      { title: "2024年生日绘", image: require('@/assets/images/return.png') },
     ]
   }
 }
