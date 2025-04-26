b86b9d5f21c778a4a84f3b691b922c285a8012fe
tobenot <42083410+tobenot@users.noreply.github.com>
2025-04-26 15:44:35 +0800

    pref: painting webp

--- src/config/paintingsConfig.js
+++ src/config/paintingsConfig.js
@@ -12,7 +12,7 @@ export default {
       {
         id: "p1",
         titleKey: { zh: "《回归》", en: "'Return'" },
-        image: require('@/assets/images/paintings/DRAW_20241022_return.png'),
+        image: require('@/assets/images/paintings/DRAW_20241022_return.webp'),
         descriptionKey: { zh: "2024年生日绘，致敬心灵杀手2。", en: "2024 Birthday Drawing, Tribute to Alan Wake 2." },
         date: { year: 2024 },
         medium: { zh: "数字绘画", en: "Digital Painting"}
