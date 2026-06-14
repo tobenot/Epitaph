5ac1be30ace743b3a42bd3524c8b404e13856fc8
tobenot <42083410+tobenot@users.noreply.github.com>
2026-06-14 17:09:45 +0800

    Update sanctuary-of-writers.js

--- src/config/projects/sanctuary-of-writers.js
+++ src/config/projects/sanctuary-of-writers.js
@@ -10,8 +10,8 @@ export default {
   },
   image: require('@/assets/images/teacat.webp'),
   descriptionKey: {
-    zh: "博客，【成为人类】事件的主要促进者。",
-    en: "Blog, the main promoter of the 'Becoming Human' event."
+    zh: "博客。",
+    en: "Blog."
   },
   date: { year: 2023, month: 11 },
   genres: ["博客", "个人网站"],
@@ -23,8 +23,8 @@ export default {
     { zh: "两个粉丝群：独立游戏群 80 人，VRChat群 11 人", en: "Two fan groups: Indie Game (80 members), VRChat (11 members)" }
   ],
   developerNotesKey: {
-    zh: "主要阵地之一。小说的读者都是个位数，发布在微信公众号和个人博客。担心发到其他平台被吃版权，想干干净净的。博客是【成为人类】事件的主要促进者。B站目前1900粉丝，缓慢增长中，会重回两千的。",
-    en: "One of the main bases. Novels have single-digit readers, published on WeChat Official Account and personal blog to avoid losing copyright to mainstream platforms. The blog is the main promoter of the 'Becoming Human' event. Bilibili currently has 1,900 followers, growing slowly, will return to 2,000."
+    zh: "主要阵地之一。小说的读者都是个位数，发布在微信公众号和个人博客。担心发到其他平台被吃版权，想干干净净的。B站目前1900粉丝，缓慢增长中，会重回两千的。",
+    en: "One of the main bases. Novels have single-digit readers, published on WeChat Official Account and personal blog to avoid losing copyright to mainstream platforms. Bilibili currently has 1,900 followers, growing slowly, will return to 2,000."
   },
   tags: ["博客", "主阵地"],
   longDescriptionKey: {
